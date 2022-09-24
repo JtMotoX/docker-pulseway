eff5152782a6b9c2cbaeda149d76609c67c701d8
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-09-23 23:12:02 -0700

    add support for 1password integration

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+# CONVERT 1PASSWORD ENVIRONMENT VARIABLES
+if env | grep -E '^[^=]*=OP:' >/dev/null; then
+	curl -sS -o /tmp/1password-vars.sh "https://raw.githubusercontent.com/JtMotoX/1password-docker/main/1password/op-vars.sh"
+	chmod 755 /tmp/1password-vars.sh
+	. /tmp/1password-vars.sh || exit 1
+	rm -f /tmp/1password-vars.sh
+fi
+
 # REPLACE VARIABLES IN CONFIG FILE
 if ! test -f "/etc/pulseway/config-template.xml"; then
 	echo "You must mount your config.xml to '/etc/pulseway/config-template.xml'"
